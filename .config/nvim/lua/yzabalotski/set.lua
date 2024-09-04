37c000348c0688cf76c8c614b7ab7cb2d0ac48ee
yzabalotski <yzabalotski@gmail.com>
2024-09-04 16:59:57 +0300

    Configure nvim via lua scripts (#4)

---
+++ .config/nvim/lua/yzabalotski/set.lua
@@ -0,0 +1,18 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.colorcolumn = "120"
+
+vim.opt.background = "dark"
