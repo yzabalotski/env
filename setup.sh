547282c6d62b1f49bddc7b6361ffaedcb2defe73
yzabalotski <yzabalotski@gmail.com>
2025-02-19 10:36:30 +0300

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 sudo dnf install -y \
 	zsh \
 	neovim \
-	google-chrome 
+	google-chrome \
+    kubernetes-client \
+    libtool \
+    openssl-devel
 
 # TODO: add gnome-tweaks 
 
 cd
 
 ln -s $(pwd)/projects/env/.config/nvim .config/nvim
+
+git config --global core.editor nvim
+
+mkdir work
